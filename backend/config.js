755044166c67bdb059658232f65301471a6ed9ac
Chinmaya <b122044@iiit-bh.ac.in>
2024-07-17 11:44:10 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const mongoDBURL="mongodb+srv://cksahoo704:6wOWeacf4lHjcN0k@cluster0.msfsscs.mongodb.net/brook?retryWrites=true&w=majority&appName=Cluster0"
+export const PORT=3001;
+
+// 6wOWeacf4lHjcN0k
---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+// import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Create from './pages/Create'
+import Edit from './pages/Edit'
+import Delete from './pages/Delete'
+import Show from './pages/Show'
+
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<Create/>}/>
+      <Route path='/books/details/:id' element={<Show/>}/>
+      <Route path='/books/edit/:id' element={<Edit/>}/>
+      <Route path='/books/delete/:id' element={<Delete/>}/>
+    </Routes>
+  )
+}
+
+export default App
